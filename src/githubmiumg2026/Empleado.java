2244f4a30b8180f102c727f6377c2d0fdcbfa4de
umg <umg@JOC-LAB-1H3.labjoc.local>
2026-04-14 19:06:36 -0600

    Clase empleado que manejara informacion del objeto empleado

---
+++ src/githubmiumg2026/Empleado.java
@@ -0,0 +1,88 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package githubmiumg2026;
+
+/**
+ *
+ * @author umg
+ */
+public class Empleado {
+    private int id;
+    private String nombre;
+    private String apellido;
+    private String direccion;
+
+    public Empleado() {
+        this.id = 0;
+        this.nombre = "";
+        this.apellido = "";
+        this.direccion = "";
+    }
+
+    public Empleado(int id, String nombre, String apellido, String direccion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.direccion = direccion;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the apellido
+     */
+    public String getApellido() {
+        return apellido;
+    }
+
+    /**
+     * @param apellido the apellido to set
+     */
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    /**
+     * @return the direccion
+     */
+    public String getDireccion() {
+        return direccion;
+    }
+
+    /**
+     * @param direccion the direccion to set
+     */
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    
+    
+}
